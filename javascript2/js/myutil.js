613737c4658e8bc10cbf78ccc078a640b4914cfa
jangjeongha <jjha0125@nate.com>
2024-06-05 10:41:21 +0900

    20240605

---
+++ javascript2/js/myutil.js
@@ -0,0 +1,10 @@
+// js.myutil.js 파일
+
+ //매개변수로 전달된 선택자를 이용해서
+ function $(sel){
+    //해당 선택자에 부합하는 문서 개게의 참조값을 리턴하는 함수
+        return document.querySelector(sel);
+    }
+    function $$(sel){
+        return document.querySelectorAll(sel);
+    }
